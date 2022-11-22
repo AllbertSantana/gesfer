d26ac64e5847745c1cf7be48ac15277b30d5c762
timeTraveler55 <andersonrog@outlook.com>
2022-11-22 17:19:56 -0300

    new commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,13 +9,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public funcionarios?: Funcionario[];
 
+  arguments: FiltroFuncionario = {
+    nome: '',
+    cpf: '',
+    matricula: ''
+  };
+
   constructor(http: HttpClient) {
-    http.get<Funcionario[]>('/api/funcionario').subscribe(result => {
-      this.funcionarios = result;
-    }, error => console.error(error));
+    http.get<PlanilhaFuncionario>('/api/funcionario', {
+      params: new HttpParams()
+        .set('pageNumber', 1)
+        .set('pageSize', 5)
+        .set('orderBy', 'id')
+        .set('order', 'asc')
+    })
+      .subscribe(result => {
+        this.funcionarios = result.results;
+      }, error => console.error(error));
   }
 
   title = 'frontend';
+}
+
+interface PlanilhaFuncionario {
+  results: Funcionario[];
+}
+
+interface FiltroFuncionario {
+  nome: string;
+  cpf: string;
+  matricula: string;
+}
+
+interface ConsultaFuncionario {
+  pageNumber: number;
+  pageSize: number;
+  arguments: FiltroFuncionario;
+  orderBy: string;
+  order: string;
 }
 
 interface Funcionario {
